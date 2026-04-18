59032c2625f8cd4de61f6ea2d8806bb0a605a8aa
nhungee <nguyentuyetnhungthtt@gmail.com>
2026-04-18 11:30:38 +0700

    CPP

--- TRR1001.cpp
+++ TRR1001.cpp
@@ -5,7 +5,6 @@ int main(){
     freopen("DT.INP", "r", stdin);
     freopen("DT.OUT", "w", stdout);
 	int t; cin >> t;
-	int res = 0;
 	if(t == 1){
 		int n; cin >> n ; 
 		int a[n][n];
@@ -21,23 +20,24 @@ int main(){
 				if(a[i][j]) deg++;
 			}
 			cout << deg << " ";
-			res += deg;
 		}
 		cout << endl;
 	}
 	if(t == 2){
 		int n; cin >> n;
 		int a[n][n];
+		int res = 0;
 		for(int i = 0; i < n ; i++){
 			for(int j =0; j < n ;j++){
 				cin >> a[i][j];
+				if(a[i][j]) res++;
 			}
 		}
 		int m = res/2;
 		cout << n <<  " " << m << endl;
 		for(int  i = 0; i < n; i++){
 			for(int j = i+1; j < n; j++){
-				if(a[i][j]) cout << i <<" " << j << endl;
+				if(a[i][j]) cout << i + 1 << " " << j + 1 << endl;
 			}
 		}
 	}
